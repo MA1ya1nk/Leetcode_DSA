14b7be9db11b41ee47f36ff8407b4558f0a96943
Mayank <114399033+MA1ya1nk@users.noreply.github.com>
2026-02-22 16:55:16 +0530

    Time: 10 ms (44.35%) | Memory: 30.7 MB (86.84%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& vis){
+    void solve(int i, int j, vector<vector<int>>& grid){
         int m=grid.size(), n = grid[0].size();
         vector<int> row = {1,0,0,-1};
         vector<int> col = {0,1,-1,0};
@@ -12,42 +12,41 @@ public:
             int currRow = top.first, currCol = top.second;
             for(int i=0;i<4;i++){
                 int newRow = currRow+row[i], newCol = currCol+col[i];
-                if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && grid[newRow][newCol]==1
-                    && !vis[newRow][newCol]){
+                if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && grid[newRow][newCol]==1){
                         q.push({newRow, newCol});
-                        vis[newRow][newCol]=1;
+                        grid[newRow][newCol]=0;
                     }
             }
         }
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size(), ans=0;
-        vector<vector<int>>vis(m, vector<int>(n,0));
+        
         for(int i=0;i<m;i++){
-            if(grid[i][0]==1 && !vis[i][0]){
-               vis[i][0]=1;
-               solve(i,0,grid,vis);
+            if(grid[i][0]==1){
+               grid[i][0]=0;
+               solve(i,0,grid);
             }
-            if(grid[i][n-1]==1 && !vis[i][n-1]){
-               vis[i][n-1]=1;
-               solve(i,n-1,grid,vis);  
+            if(grid[i][n-1]==1){
+               grid[i][n-1]=0;
+               solve(i,n-1,grid);  
             } 
         }
 
         for(int i=0;i<n;i++){
-            if(grid[0][i]==1 && !vis[0][i]){
-               vis[0][i]=1;
-               solve(0,i,grid,vis);
+            if(grid[0][i]==1){
+               grid[0][i]=0;
+               solve(0,i,grid);
             }
-            if(grid[m-1][i]==1 && !vis[m-1][i]){
-               vis[m-1][i]=1;
-               solve(m-1,i,grid,vis);  
+            if(grid[m-1][i]==1){
+               grid[m-1][i]=0;
+               solve(m-1,i,grid);  
             } 
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1 && !vis[i][j]) ans++;
+                if(grid[i][j]==1) ans++;
             }
         }
 
